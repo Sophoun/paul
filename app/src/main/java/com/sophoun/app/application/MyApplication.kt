47a5607c732157a4434adfb9f8268a6bd540c0ec
Sophoun <n-sophoun@beniten.com>
2020-06-28 20:05:43 +0700

    Test Android Log class

--- app/src/main/java/com/sophoun/app/application/MyApplication.kt
+++ app/src/main/java/com/sophoun/app/application/MyApplication.kt
@@ -1,11 +1,15 @@
 package com.sophoun.app.application
 
 import android.app.Application
+import com.sophoun.utils.DLog
+import com.sophoun.utils.DLog.Level.DEBUG
 
 
 class MyApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        DLog.level(DEBUG)
+        DLog.d("Application class onCreate()")
     }
 }
